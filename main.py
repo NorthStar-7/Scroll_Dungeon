07e3d581abd65551fd91a4666bd1d0fcdd201922
CommanderEpik <alaaskander2@GMAIL.COM>
2025-03-16 14:19:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -31,16 +31,48 @@ class Player:
         if pyxel.btn(KEY_LEFT) and self.x > 0:
             self.x -= 2
         
-class Environnement:
-    def __init__(self, x, y, size, element_type):
+class Plateforme:
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.size = size
-        self.element = element_type
-        self.spawntime = pyxel.frame_count
-        self.activated = False #For trap objects
+        self.largeur = 78  
 
-def universal_scroll(object):
-    self.y -= 1
-    sigma sigma boy
+    def update(self):
+        self.y += 2.5  # Vitesse de descente de la plateforme
+        if self.y > 650:
+            y_min = min(plat.y for plat in plateformes if plat.y < 650)  
+            self.y = y_min - random.randint(40, 80)
+            self.x = random.randint(10, 300 - self.largeur - 10)
+
+    def draw(self):
+        pyxel.rect(self.x, self.y, self.largeur, 5, 7) 
+
+class Enemies:
+    def __init__(self, x):
+        self.x = x
+        self.y = random.randint(-100, -50)
+        self.taille = 16
+        self.vitesse_descente = 5
+
+    def update(self):
+        self.y += self.vitesse_descente
+        if self.y > 650:
+            self.y = random.randint(-100, -50)
+            self.x = random.randint(10, 300 - self.taille) 
+
+    def draw(self):
+        pyxel.circ(self.x + self.taille // 2, self.y + self.taille // 2, self.taille // 2, 9)
+
+    def verifier_collision(self, joueur):
+        collision_en_x = (self.x < joueur.x + joueur.size) and (self.x + self.taille > joueur.x)
+        collision_en_y = (self.y < joueur.y + joueur.size) and (self.y + self.taille > joueur.y)
+
+        return collision_en_x and collision_en_y
+
+
+# Initialisation du joueur et des plateformes
+joueur = Joueur()
+plateformes = [Plateforme(random.randint(10, 220), random.randint(100, 550)) for _ in range(8)]
+enemies = [Enemies(random.randint(10, 300)) for _ in range(3)] 
+
 
