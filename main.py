0fa657f71baabc9d370e7a669b2806fcf28201ba
NorthStar-7 <hajji.ismail7655@gmail.com>
2025-03-16 14:11:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,23 +8,33 @@ class Player:
         self.size = 20
         self.x = 150
         self.y = 150
-        self.action = ["resting", 0] #
-        '''resting, jumping or falling as a string  will determine the player's vertical movement
-        the number is the action timer (amount of frames player has jumped or fell) to deal with jumping or falling speed
-        '''
-        self.surplateforme = False #will immediatly put the action at resting when True
+        self.vy = 0
+        self.action = "resting" 
+        '''resting, jumping or falling as a string  will determine the player's vertical movement'''
         self.alive = True
 
     def update_position(self):
+
+        #To do; check if the player is resting on any platform by going through the list of platforms; put the action as resting if anything is true. 
+        
         if self.action[0] == "resting":
-            universal_scroll(self)
+            self.vy = -1
+            if pyxel.btn(KEY_UP): #Jumping when at rest will change the action and falling value
+                self.action = "jumping"
+                self.vy = 6
 
-        elif self.action[0] == "falling":
-            self.y -= 3
-
-        elif self.action[0] == "jumping":
-            self.y += 3
+        if self.action[0] == "jumping": #What to do with the falling value when jumping
+            if self.vy < 0:
+                self.vy -= 1
+            else:
+                self.action = "falling"
             
+        if self.action[0] == "falling":
+            if self.vy > 5:
+                self.vy -= 1
+
+        self.y += self.vy #After all actions and calculations have been made, actually change the y value.
+        
         if pyxel.btn(KEY_RIGHT) and self.x < 300 - self.size:
             self.x += 2
 
