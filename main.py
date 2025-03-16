9cc97e548c6448b77a8d19fd4376d5c5e030ddaa
NorthStar-7 <hajji.ismail7655@gmail.com>
2025-03-16 13:35:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pyxel.init(300, 500, title="Scroll Dungeon")
 
 class Player:
     def __init__(self):
-        self.taille = 20
+        self.size = 20
         self.x = 150
         self.y = 150
         self.action = ["resting", 0] #
@@ -15,20 +15,27 @@ class Player:
         self.surplateforme = False #will immediatly put the action at resting when True
         self.alive = True
 
-    def update(self):
+    def update_position(self):
         if self.action[0] == "resting":
             universal_scroll(self)
 
-        if pyxel.btn(KEY_RIGHT) and self.x < 300 - self.taille:
+        elif self.action[0] == "falling":
+            self.y -= 3
+
+        elif self.action[0] == "jumping":
+            self.y += 3
+            
+        if pyxel.btn(KEY_RIGHT) and self.x < 300 - self.size:
             self.x += 2
 
         if pyxel.btn(KEY_LEFT) and self.x > 0:
             self.x -= 2
         
 class Environnement:
-    def __init__(self, x, y, element_type):
+    def __init__(self, x, y, size, element_type):
         self.x = x
         self.y = y
+        self.size = size
         self.element = element_type
         self.spawntime = pyxel.frame_count
         self.activated = False #For trap objects
