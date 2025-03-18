5c50d8323a099b334c3766961424f4eed9dc7cc1
NorthStar-7 <hajji.ismail7655@gmail.com>
2025-03-18 22:43:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,42 +5,27 @@ pyxel.init(300, 500, title="Scroll Dungeon")
 
 class Player:
     def __init__(self):
-        self.size = 20
-        self.x = 150
-        self.y = 150
-        self.vy = 0
-        self.action = "resting" 
-        '''resting, jumping or falling as a string  will determine the player's vertical movement'''
-        self.alive = True
+        self.taille = 16
+        self.x = 300 // 2 - self.taille//2
+        self.y = 500 // 2 - self.taille//2
+        self.vy = 0 
+        self.sur_plateforme = False
 
     def update_position(self):
+        if self.surplateforme and pyxel.btn(KEY_SPACE)
+            self.vy = -8
 
-        #To do; check if the player is resting on any platform by going through the list of platforms; put the action as resting if anything is true. 
+        self.y+=self.vy
+        if self.vy<8:
+            self.vy += 0.5
+    
+        if pyxel.btn(pyxel.KEY_RIGHT) and self.x < 300-self.taille:
+            self.x += 3
+        if pyxel.btn(pyxel.KEY_LEFT) and self.x > 0:
+            self.x += -3
+
         
-        if self.action[0] == "resting":
-            self.vy = -1
-            if pyxel.btn(KEY_UP): #Jumping when at rest will change the action and falling value
-                self.action = "jumping"
-                self.vy = 6
-
-        if self.action[0] == "jumping": #What to do with the falling value when jumping
-            if self.vy < 0:
-                self.vy -= 1
-            else:
-                self.action = "falling"
-            
-        if self.action[0] == "falling":
-            if self.vy > 5:
-                self.vy -= 1
-
-        self.y += self.vy #After all actions and calculations have been made, actually change the y value.
-        
-        if pyxel.btn(KEY_RIGHT) and self.x < 300 - self.size:
-            self.x += 2
-
-        if pyxel.btn(KEY_LEFT) and self.x > 0:
-            self.x -= 2
-        
+   '''     
 class Environnement:
     def __init__(self, x, y, size, element_type):
         self.x = x
@@ -49,7 +34,4 @@ class Environnement:
         self.element = element_type
         self.spawntime = pyxel.frame_count
         self.activated = False #For trap objects
-
-def universal_scroll(object):
-    self.y -= 1
-
+'''
